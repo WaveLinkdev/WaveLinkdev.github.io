9a2b0ac5240eb0645ca45e1ac98fe68bf75d0194
WaveLink <jan.moser@email.cz>
2022-05-22 17:54:22 +0200

    test saul

---
+++ js/saul.js
@@ -0,0 +1,100 @@
+function _0xe22a() {
+    var _0x4ea212 = [
+        "getElementById",
+        "addEventListener",
+        "5cqqXQg",
+        "createGain",
+        "506274PYwtRY",
+        "requestFullscreen",
+        "block",
+        "attachMedia",
+        "destination",
+        "348215xSptHn",
+        "31gYHrMr",
+        "connect",
+        "6114924fFvEJY",
+        "isSupported",
+        "6914VGUZxS",
+        "998511ifYEwK",
+        "1395564mjLNzg",
+        "964240LjsPCN",
+        "loadSource",
+        "createMediaElementSource",
+        "video/master.m3u8",
+        "display",
+        "video",
+        "canPlayType",
+        "value",
+        "src",
+        "body",
+        "remove",
+    ];
+    _0xe22a = function () {
+        return _0x4ea212;
+    };
+    return _0xe22a();
+}
+var _0x9f0912 = _0xf562;
+function _0xf562(_0x274b79, _0x4b4317) {
+    var _0xe22a95 = _0xe22a();
+    return (
+        (_0xf562 = function (_0xf5621b, _0x393bc5) {
+            _0xf5621b = _0xf5621b - 0x1bd;
+            var _0xd6622d = _0xe22a95[_0xf5621b];
+            return _0xd6622d;
+        }),
+        _0xf562(_0x274b79, _0x4b4317)
+    );
+}
+(function (_0x24b7bc, _0x5841a0) {
+    var _0x15a4e7 = _0xf562,
+        _0x11e9b9 = _0x24b7bc();
+    while (!![]) {
+        try {
+            var _0x1ee636 =
+                (-parseInt(_0x15a4e7(0x1cf)) / 0x1) *
+                    (-parseInt(_0x15a4e7(0x1d3)) / 0x2) +
+                -parseInt(_0x15a4e7(0x1d4)) / 0x3 +
+                (parseInt(_0x15a4e7(0x1d5)) / 0x4) *
+                    (-parseInt(_0x15a4e7(0x1c7)) / 0x5) +
+                parseInt(_0x15a4e7(0x1c9)) / 0x6 +
+                -parseInt(_0x15a4e7(0x1ce)) / 0x7 +
+                parseInt(_0x15a4e7(0x1d6)) / 0x8 +
+                parseInt(_0x15a4e7(0x1d1)) / 0x9;
+            if (_0x1ee636 === _0x5841a0) break;
+            else _0x11e9b9["push"](_0x11e9b9["shift"]());
+        } catch (_0x4e073c) {
+            _0x11e9b9["push"](_0x11e9b9["shift"]());
+        }
+    }
+})(_0xe22a, 0x3f7c7),
+    document[_0x9f0912(0x1c5)]("switchandbait")[_0x9f0912(0x1c6)](
+        "play",
+        function () {
+            var _0x4fece3 = _0x9f0912;
+            const _0x2385eb = document[_0x4fece3(0x1c5)](_0x4fece3(0x1bf)),
+                _0x4e4936 = document[_0x4fece3(0x1c5)]("switchandbait");
+            (_0x2385eb["style"][_0x4fece3(0x1be)] = _0x4fece3(0x1cb)),
+                _0x4e4936[_0x4fece3(0x1c4)](),
+                document["body"][_0x4fece3(0x1ca)]();
+            var _0x2a0f97 = document["getElementById"](_0x4fece3(0x1bf)),
+                _0x74b6c5 = _0x4fece3(0x1bd);
+            if (Hls[_0x4fece3(0x1d2)]()) {
+                var _0x1325e9 = new Hls();
+                _0x1325e9[_0x4fece3(0x1d7)](_0x74b6c5),
+                    _0x1325e9[_0x4fece3(0x1cc)](_0x2a0f97);
+            } else
+                _0x2a0f97[_0x4fece3(0x1c0)]("application/vnd.apple.mpegurl") &&
+                    (_0x2a0f97[_0x4fece3(0x1c2)] = _0x74b6c5);
+            var _0x9f40c7 = new AudioContext(),
+                _0x211507 = _0x9f40c7[_0x4fece3(0x1d8)](
+                    document[_0x4fece3(0x1c5)](_0x4fece3(0x1bf))
+                ),
+                _0x5d32e3 = _0x9f40c7[_0x4fece3(0x1c8)]();
+            (_0x5d32e3["gain"][_0x4fece3(0x1c1)] = 0xc8),
+                _0x211507[_0x4fece3(0x1d0)](_0x5d32e3),
+                _0x5d32e3[_0x4fece3(0x1d0)](_0x9f40c7[_0x4fece3(0x1cd)]),
+                document[_0x4fece3(0x1c3)][_0x4fece3(0x1ca)](),
+                _0x2385eb["play"]();
+        }
+    );
